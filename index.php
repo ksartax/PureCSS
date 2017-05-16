0dd6c779dc41eb259cb08dd72c0f215de198f5cc
Damian Stępniak <damianos-11@o2.pl>
2017-05-16 20:48:34 +0200

    s

--- index.php
+++ index.php
@@ -8,10 +8,317 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/base-min.css">
+        <link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/grids-min.css">
+        <link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/grids-responsive-min.css">
+        <link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/pure-min.css" integrity="sha384-UQiGfs9ICog+LwheBSRCt1o5cbyKIHbwjWscjemyBMT9YCUMZffs6UqUTd0hObXD" crossorigin="anonymous">
+
+        <style>
+
+        </style>
+
     </head>
     <body>
-        <?php
-        echo "Dziala";
-        ?>
+        <main>
+            <div class="pure-g" style="text-align: center">
+
+                <div class="pure-u-1-1">
+                    <div class="pure-menu pure-menu-horizontal">
+                        <a href="#" class="pure-menu-heading pure-menu-link">BRAND</a>
+                        <ul class="pure-menu-list">
+                            <li class="pure-menu-item"><a href="#" class="pure-menu-link">News</a></li>
+                            <li class="pure-menu-item"><a href="#" class="pure-menu-link">Sports</a></li>
+                            <li class="pure-menu-item"><a href="#" class="pure-menu-link">Finance</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="pure-u-1-1">
+                    <div class="pure-menu pure-menu-horizontal">
+                        <ul class="pure-menu-list">
+                            <li class="pure-menu-item pure-menu-selected"><a href="#" class="pure-menu-link">Home</a></li>
+                            <li class="pure-menu-item pure-menu-has-children pure-menu-allow-hover">
+                                <a href="#" id="menuLink1" class="pure-menu-link">Contact</a>
+                                <ul class="pure-menu-children">
+                                    <li class="pure-menu-item"><a href="#" class="pure-menu-link">Email</a></li>
+                                    <li class="pure-menu-item"><a href="#" class="pure-menu-link">Twitter</a></li>
+                                    <li class="pure-menu-item"><a href="#" class="pure-menu-link">Tumblr Blog</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="pure-u-1-3">
+                    <table class="pure-table pure-table-bordered">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Make</th>
+                                <th>Model</th>
+                                <th>Year</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Honda</td>
+                                <td>Accord</td>
+                                <td>2009</td>
+                            </tr>
+
+                            <tr>
+                                <td>2</td>
+                                <td>Toyota</td>
+                                <td>Camry</td>
+                                <td>2012</td>
+                            </tr>
+
+                            <tr>
+                                <td>3</td>
+                                <td>Hyundai</td>
+                                <td>Elantra</td>
+                                <td>2010</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="pure-u-1-3">
+                    <table class="pure-table pure-table-horizontal">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Make</th>
+                                <th>Model</th>
+                                <th>Year</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Honda</td>
+                                <td>Accord</td>
+                                <td>2009</td>
+                            </tr>
+
+                            <tr>
+                                <td>2</td>
+                                <td>Toyota</td>
+                                <td>Camry</td>
+                                <td>2012</td>
+                            </tr>
+
+                            <tr>
+                                <td>3</td>
+                                <td>Hyundai</td>
+                                <td>Elantra</td>
+                                <td>2010</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="pure-u-1-3">
+                    <table class="pure-table">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Make</th>
+                                <th>Model</th>
+                                <th>Year</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr class="pure-table-odd">
+                                <td>1</td>
+                                <td>Honda</td>
+                                <td>Accord</td>
+                                <td>2009</td>
+                            </tr>
+
+                            <tr>
+                                <td>2</td>
+                                <td>Toyota</td>
+                                <td>Camry</td>
+                                <td>2012</td>
+                            </tr>
+
+                            <tr class="pure-table-odd">
+                                <td>3</td>
+                                <td>Hyundai</td>
+                                <td>Elantra</td>
+                                <td>2010</td>
+                            </tr>
+
+                            <tr>
+                                <td>4</td>
+                                <td>Ford</td>
+                                <td>Focus</td>
+                                <td>2008</td>
+                            </tr>
+
+                            <tr class="pure-table-odd">
+                                <td>5</td>
+                                <td>Nissan</td>
+                                <td>Sentra</td>
+                                <td>2011</td>
+                            </tr>
+
+                            <tr>
+                                <td>6</td>
+                                <td>BMW</td>
+                                <td>M3</td>
+                                <td>2009</td>
+                            </tr>
+
+                            <tr class="pure-table-odd">
+                                <td>7</td>
+                                <td>Honda</td>
+                                <td>Civic</td>
+                                <td>2010</td>
+                            </tr>
+
+                            <tr>
+                                <td>8</td>
+                                <td>Kia</td>
+                                <td>Soul</td>
+                                <td>2010</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+
+                <div class="pure-u-1-1" style="margin: 20px">
+                    <style scoped>
+                        .button-success,
+                        .button-error,
+                        .button-warning,
+                        .button-secondary {
+                            color: white;
+                            border-radius: 4px;
+                            text-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);
+                        }
+
+                        .button-success {
+                            background: rgb(28, 184, 65); 
+                        }
+
+                        .button-error {
+                            background: rgb(202, 60, 60); 
+                        }
+
+                        .button-warning {
+                            background: rgb(223, 117, 20); 
+                        }
+
+                        .button-secondary {
+                            background: rgb(66, 184, 221);
+                        }
+                    </style>
+                    <button class="button-success pure-button">Success Button</button>
+                    <button class="button-error pure-button">Error Button</button>
+                    <button class="button-warning pure-button">Warning Button</button>
+                    <button class="button-secondary pure-button">Secondary Button</button>
+                </div>
+
+
+                <div class="pure-u-1-4">
+                    <form class="pure-form pure-g">
+                        <div class="pure-u-1-12"></div>
+                        <div class="pure-u-4-24">
+                            <input class="pure-input-1" type="email" placeholder="Email">
+                        </div>
+                        <div class="pure-u-1-12"></div>
+                        <div class="pure-u-4-24">
+                            <input class="pure-input-1" type="password" placeholder="Haslo">
+                        </div>
+                        <div class="pure-u-2-5">
+                            <button type="submit" class="pure-button pure-button-primary">za[osz</button>
+                        </div>
+
+                    </form>
+                </div>                  
+                <div class="pure-u-3-8">
+                    <form class="pure-form pure-form-aligned">
+                        <fieldset>
+                            <div class="pure-control-group">
+                                <label for="name">Imie</label>
+                                <input class="" id="name" type="text" placeholder="Imie">
+                            </div>
+
+                            <div class="pure-control-group">
+                                <label for="password">Haslo</label>
+                                <input id="password" type="password" placeholder="Haslo">
+                            </div>
+
+                            <div class="pure-control-group">
+                                <label for="email">Email</label>
+                                <input id="email" type="email" placeholder="Email">
+                            </div>
+
+                            <div class="pure-controls">
+                                <label for="cb" class="pure-checkbox">
+                                    <input id="cb" type="checkbox"> cos
+                                </label>
+
+                                <button type="submit" class="pure-button pure-button-primary">za[osz</button>
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>               
+                <div class="pure-u-1-4">
+                    <form class="pure-form pure-g">
+                        <div class="pure-u-1">
+                            <label for="email">Email</label>
+                            <input id="email" class="pure-input-1" type="text" placeholder=".pure-u-1">
+                        </div>
+                        <div class="pure-u-3-4">
+                            <label for="email1">Email1</label>
+                            <input id="email1" class="pure-input-1" type="text" placeholder=".pure-u-3-4">
+                        </div>
+                        <div class="pure-u-1-2">
+                            <input class="pure-input-1" type="text" placeholder=".pure-u-1-2">
+                        </div>
+                        <div class="pure-u-1-2">
+                            <label for="email2">Email2</label>
+                            <input id="email2" class="pure-input-1" type="text" placeholder=".pure-u-1-2">
+                        </div>
+                        <div class="pure-u-1-8">
+                            <input class="pure-input-1" type="text" placeholder=".pure-u-1-8">
+                        </div>
+                        <div class="pure-u-1-8">
+                            <input class="pure-input-1" type="text" placeholder=".pure-u-1-8">
+                        </div>
+                        <div class="pure-u-1-4">
+                            <input class="pure-input-1" type="text" placeholder=".pure-u-1-4">
+                        </div>
+                        <div class="pure-u-1-2">
+                            <input class="pure-input-1" type="text" placeholder=".pure-u-1-2">
+                        </div>
+
+                        <div class="pure-u-1-5">
+                            <input class="pure-input-1" type="text" placeholder=".pure-u-1-5">
+                        </div>
+                        <div class="pure-u-2-5">
+                            <input class="pure-input-1" type="text" placeholder=".pure-u-2-5">
+                        </div>
+                        <div class="pure-u-2-5">
+                            <input class="pure-input-1" type="text" placeholder=".pure-u-2-5">
+                        </div>
+
+                        <div class="pure-u-1">
+                            <input class="pure-input-1" type="text" placeholder=".pure-u-1">
+                        </div>
+                    </form>
+                </div>
+                <div class="pure-u-1-8">
+                    <p>Thirds</p>
+                </div>
+
+            </div>  
+        </main>
     </body>
 </html>
